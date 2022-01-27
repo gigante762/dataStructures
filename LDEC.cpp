f2aedc89cec8cf77095de0f416704eeda9f4770f
Kevin Souza <igantekevin@hotmail.com>
2022-01-27 09:08:11 -0300

    funcao de remocao ok

--- LDEC.cpp
+++ LDEC.cpp
@@ -26,8 +26,8 @@ using namespace std;
     Buscar um valor na list -> retornar; ok
         Atualizar um valor da lista ok
         
-    Remover um valor da lista
-    Remover todos os valores da lista
+    Remover um valor da lista ok
+    Remover todos os valores da lista ok
     --------
 
     ----- Projeto ----- 
@@ -47,30 +47,9 @@ struct NodeD{
 
 //Lista duplamente circular
 struct ListaDEC{
-    //ponteiro para o inicio, default NULL
+    // ponteiro para o inicio, default NULL
     NodeD* first = NULL;
 };
-
-
-/* Anda mostrando os valores do front pro back */
-/*
-void describeFtoB(ListaDE* l){
-    cout << "Lista FtoB: ";
-    for (NodeD* aux = l->front; aux != NULL; aux = aux->next)
-        cout << aux->v << " ";
-    cout << endl;
-}/*
-
-/* Anda mostrando os valores do back pro front */
-/* 
-void describeBtoF(ListaDE* l){
-    cout << "Lista BtoF: ";
-    for (NodeD* aux = l->back; aux != NULL; aux = aux->prev)
-        cout << aux->v << " ";
-    cout << endl;
-}
-*/
-
 
 /*Mostra todos os valores partindo de l->first*/
 void describeAll(ListaDEC* l)
@@ -113,209 +92,6 @@ bool isFull(ListaDEC* l)
         return true;
     }
 }
-
-
-/* Add items to the end of an list */
-/*
-void push(ListaDE*& l, ItemType v)
-{
-    //Cria um novo node;
-    if (isFull(l))
-    {
-        cout << "Memoria cheia" << endl;
-        return;
-    }
-
-    NodeD* newnode = new NodeD;
-    newnode->v = v;
-
-    //caso nao seja null o back,
-    //ajudas o next pra apontar para esse novo noded
-    if (l->back != NULL)
-        l->back->next = newnode;
-
-    //ajusta os ponteiros, o novo aponta pro que ja estava,
-    // e back agora é o ultimo
-    newnode->prev = l->back;
-    l->back = newnode;
-
-    if (l->front == NULL)
-        l->front = newnode;
-    
-}
-*/
-
-/* Add items to the beginning of an array */
-/*
-void unshift(ListaDE*& l, ItemType v)
-{
-    if (isFull(l))
-    {
-        cout << "Memoria cheia" << endl;
-        return;
-    }
-
-    NodeD* newnode = new NodeD;
-    newnode->v = v;
-
-    //ajusta os ponteiros
-    newnode->next = l->front;
-
-    if (l->front != NULL)
-        l->front->prev = newnode;
-    else
-        l->back = newnode;
-
-    l->front = newnode;
-}
-*/
-
-/* Remove an item from the beginning of an array.*/
-/*
-ItemType shift(ListaDE*& l)
-{
-
-    if (!isEmpty(l))
-    {
-        ItemType r = l->front->v;
-        
-        if (l->front->next != NULL)
-        {
-            l->front = l->front->next;
-            delete l->front->prev;
-            l->front->prev = NULL;
-
-        }
-        else
-        {
-            delete l->front;
-            l->front = NULL;
-            l->back = NULL;
-        }
-
-        return r;
-    }
-
-    cout << "Lista vazia\n";
-    return false;
-    
-
-}
-*/
-
-/* Remove an item from the end of an list */
-/*
-ItemType pop(ListaDE*& l)
-{
-    if (!isEmpty(l))
-    {
-        if (l->back->prev != NULL)
-        {
-            l->back = l->back->prev;
-            ItemType r = l->back->next->v;
-            delete l->back->next;
-
-            //ajusta o ponteiro
-            l->back->next = NULL;
-
-            return r;
-        }
-
-        //else implicito
-        ItemType r = l->back->v;
-        delete l->back;
-        l->back = NULL;
-        l->front = NULL;
-
-        return r;
-    }
-    else
-    { 
-        cout << "Lista vazia\n";
-        return false;
-    }
-}
-*/
-
-/* Procura o valor v na lista e caso encontre retorna o noded
-caso contrario retorna o noded com null; */
-/*
-NodeD* findValue(ListaDE* l, ItemType v)
-{
-    for (NodeD* aux = l->front; aux != NULL; aux = aux->next)
-    {
-        if(aux->v == v)
-            return aux;
-    }
-
-    NodeD* r = NULL;
-    return r;
-}
-*/
-
-/* Insere um noded após outro noded */
-/*
-void insertAfter(ListaDEC*& l, NodeD* n, ItemType v)
-{
-    if (isFull(l))
-    {
-        cout << "Memoria cheia" << endl;
-        return;
-    }
-
-    NodeD* newnode = new NodeD;
-    newnode->v = v;
-
-    //ajusta o ponteiro do newnode;
-    newnode->prev = n;
-    newnode->next = newnode->prev->next;
-
-    // verifico se é o noded é o back da lista
-    if (l->back == n)
-        l->back = newnode;
-
-    //ajusta o found
-    n->next = newnode;
-
-    //ajusta o 'da frente'
-    if (newnode->next)
-        newnode->next->prev = newnode;
-}
-*/
-
-/* Insere um noded antes outro noded */
-/*
-void insertBefore(ListaDE*& l, NodeD* n, ItemType v)
-{
-    if (isFull(l))
-    {
-        cout << "Memoria cheia" << endl;
-        return;
-    }
-
-    NodeD* newnode = new NodeD;
-    newnode->v = v;
-
-    newnode->next = n;
-    newnode->prev = n->prev;
-
-    if (newnode->prev != NULL)
-    {
-        newnode->prev->next = newnode;
-
-    }
-
-    n->prev = newnode;
-
-    if (l->front == n)
-        l->front = newnode;
-    
-}
-*/
-
-
-
-
 
 /*Funçõe para a Lista Duplamente Encadeada Circular*/
 
@@ -364,6 +140,11 @@ void insert(ListaDEC*& l,  ItemType v)
 NodeD* buscarValor(ListaDEC* l, ItemType v)
 {
     NodeD* result = NULL;
+
+    if (l->first == NULL)
+    {
+        return result;
+    }
     
     NodeD* aux = l->first;
     
@@ -374,7 +155,6 @@ NodeD* buscarValor(ListaDEC* l, ItemType v)
         }
          aux = aux->next;
     }while(aux != l->first);
-    
     
     return result;
 }
@@ -393,31 +173,49 @@ NodeD* atualizarValor(ListaDEC*& l, ItemType v, ItemType a)
 
 bool removeValor(ListaDEC*& l, ItemType v)
 {
-    
     NodeD* result = buscarValor(l, v);
-    
+
     if (!result)
     {
+        cout << "Valor '" << v << "' nao localizado para remocao\n";
         return false;
     }
     
-    
-    
-    // remoção do elemento da ponta direita...    
-    if (result->next == l->first)
+    // remoção do elemento da ponta direita...
+    if (result->next == l->first && result != l->first)
     {
         l->first->prev = result->prev;
         result->prev->next = l->first;
         return true;
     }
     
-    // remoção do elemento da ponta direita...
-    
+    // remoção do elemento first...
+    // verificar se há outro ou só um elemento na lista
+    if (result == l->first)
+    {
+        //verifica se há outro além dele, nesse caso remove se só há um;
+        if (result->next == l->first && result->prev == l->first)
+        {
+            
+            delete l->first;
+            l->first = NULL;
+            return true;
+        }
+        else
+        {
+            //faz o first ser o proximo da lista;
+            l->first = l->first->next;
+
+            // como agora ele não é o first pode chamar o método novamente para cuidar
+            // dessa remoção, ou será meio ou direita;
+            return removeValor(l, v);
+        }
+    }
+
     
     // caso de remoção de elemento ao meio
     result->next->prev = result->prev;
     result->prev->next = result->next;
-    
     
     delete result;
     
@@ -430,17 +228,25 @@ int main()
 {
     ListaDEC* listaDEC = new ListaDEC;
     
+    describeAll(listaDEC);
     insert(listaDEC, 'A');
     insert(listaDEC, 'B');
     insert(listaDEC, 'C');
-    
-    cout << removeValor(listaDEC, 'B') << endl;
+
     
     describeAll(listaDEC);
-    
-    
-    
+    cout << removeValor(listaDEC, 'A') << endl;
+    describeAll(listaDEC);
+    cout << removeValor(listaDEC, 'B') << endl;
+    describeAll(listaDEC);
+    cout << removeValor(listaDEC, 'C') << endl;
+    describeAll(listaDEC);
 
+    cout << removeValor(listaDEC, 'C') << endl;
+    describeAll(listaDEC);
+    
+   
+    //fim do programa, vai liberar toda a memória
     delete listaDEC;
     return 0;
 }
